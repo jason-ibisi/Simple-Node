52d4c5be93c36614955e8f56cd97975acbd760cf
jason-ibisi <jasonibisi@gmail.com>
2022-07-23 14:50:44 +0100

    refactor: added environment variables

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
+
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
-    console.log('Containers rule!');
+  while (true) {
+    console.log(
+      `Containers and Microservices rule! ${process.env.FOVORITE_FOOD} rocks!`
+    );
     await sleep(5000);
   }
 }
